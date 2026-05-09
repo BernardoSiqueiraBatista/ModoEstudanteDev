aa5e0a3e64e92715b541196ac00c0ae6e2cfb17f
maqvn <maqvn@cin.ufpe.br>
2026-05-08 21:15:24 -0300

    alteração de caminho em arquivos de test tipo jest

--- apps/HipocratesAi-BackEnd/jest.student.config.cjs
+++ apps/HipocratesAi-BackEnd/jest.student.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   // Garante que o Jest procure os testes na pasta correta
-  testMatch: ['**/studentTests/**/*.test.ts'],
+  testMatch: ['/src/modules/**/*.test.ts'],
   // Ajuda o TS a resolver módulos caso você use aliases (@/)
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
