d8f4e1187fc73bd64d21687e6260d565d928ddef
maqvn <maqvn@cin.ufpe.br>
2026-05-14 20:11:06 -0300

    Rotas da feature 1 semana 11/05

--- apps/HipocratesAi-BackEnd/src/modules/student/student.routes.ts
+++ apps/HipocratesAi-BackEnd/src/modules/student/student.routes.ts
@@ -2,11 +2,16 @@ import { Router } from 'express';
 import performanceRoutes from '../performance/performance.routes';
 import questionsRoutes from '../questions/questions.routes';
 import examsRoutes from '../exams/exams.routes';
+import dashboardRoutes from '../student-dashboard/student-dashboard.routes';
+import insightsRoutes from '../insights/insights.routes';
 
 const studentRouter = Router();
 
 studentRouter.use('/exams', examsRoutes);
 studentRouter.use('/questions', questionsRoutes);
 studentRouter.use('/performance', performanceRoutes);
+
+studentRouter.use('/:id/', dashboardRoutes);
+studentRouter.use('/:id/insights', insightsRoutes);
 
 export default studentRouter;
